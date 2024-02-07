e828392bf588161ea5641b6713933d1331f228b1
KristanPanton <43457073+KristanPanton@users.noreply.github.com>
2024-02-07 16:13:23 -0500

    first commit

---
+++ recursive-insertion-sort.py
@@ -0,0 +1,19 @@
+def recursive_insertion_sort(arr, n):
+    if n <= 1:
+        return
+    
+    recursive_insertion_sort(arr, n - 1)
+
+    key = arr[n - 1]
+    j = n - 2
+
+    while j >= 0 and arr[j] < key:
+        arr[j+1] = arr[j]
+        j -= 1
+
+    arr[j+1] = key
+
+arr = [12, 11, 13, 5, 6]
+n = len(arr)
+recursive_insertion_sort(arr, n)
+print(arr)
